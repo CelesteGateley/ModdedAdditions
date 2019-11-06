d948050f6ca4a510a8300f555888ed935a53986b
Celeste Magisteel <celestemagisteel@gmail.com>
2019-11-06 16:58:32 +0000

    Changed Vacuum Command from using ModdedAdditions instance to MagnetInstanceController

--- src/main/java/xyz/fluxinc/moddedadditions/commands/VacuumCommand.java
+++ src/main/java/xyz/fluxinc/moddedadditions/commands/VacuumCommand.java
@@ -4,14 +4,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import xyz.fluxinc.moddedadditions.ModdedAdditions;
+import xyz.fluxinc.moddedadditions.controllers.MagnetInstanceController;
 
 public class VacuumCommand implements CommandExecutor {
 
-    private ModdedAdditions instance;
+    private MagnetInstanceController magnetInstanceController;
 
-    public VacuumCommand(ModdedAdditions instance) {
-        this.instance = instance;
-    }
+    public VacuumCommand(MagnetInstanceController magnetInstanceController) { this.magnetInstanceController = magnetInstanceController; }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
