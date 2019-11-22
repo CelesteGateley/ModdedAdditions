f365e59b28957e07d29cdbe4629e4d6ca93f002b
CelesteMagisteel <celestemagisteel@gmail.com#>
2019-11-22 15:45:46 +0000

    Moved Tools out of VeinMinerListener to Tools

--- src/main/java/xyz/fluxinc/moddedadditions/listeners/VeinMinerListener.java
+++ src/main/java/xyz/fluxinc/moddedadditions/listeners/VeinMinerListener.java
@@ -16,44 +16,12 @@ import xyz.fluxinc.moddedadditions.controllers.VeinMinerController;
 import java.util.ArrayList;
 import java.util.List;
 
+import static xyz.fluxinc.moddedadditions.storage.Tools.*;
+
 public class VeinMinerListener implements Listener {
 
     private BlockAccessController accessController;
     private VeinMinerController vmController;
-
-    private static List<Material> pickaxes;
-    private static List<Material> axes;
-    private static List<Material> shovels;
-    private static List<Material> hoes;
-    static {
-        pickaxes = new ArrayList<>();
-        pickaxes.add(Material.WOODEN_PICKAXE);
-        pickaxes.add(Material.STONE_PICKAXE);
-        pickaxes.add(Material.IRON_PICKAXE);
-        pickaxes.add(Material.GOLDEN_PICKAXE);
-        pickaxes.add(Material.DIAMOND_PICKAXE);
-
-        axes = new ArrayList<>();
-        axes.add(Material.WOODEN_AXE);
-        axes.add(Material.STONE_AXE);
-        axes.add(Material.IRON_AXE);
-        axes.add(Material.GOLDEN_AXE);
-        axes.add(Material.DIAMOND_AXE);
-
-        shovels = new ArrayList<>();
-        shovels.add(Material.WOODEN_SHOVEL);
-        shovels.add(Material.STONE_SHOVEL);
-        shovels.add(Material.IRON_SHOVEL);
-        shovels.add(Material.GOLDEN_SHOVEL);
-        shovels.add(Material.DIAMOND_SHOVEL);
-
-        hoes = new ArrayList<>();
-        hoes.add(Material.WOODEN_HOE);
-        hoes.add(Material.STONE_HOE);
-        hoes.add(Material.IRON_HOE);
-        hoes.add(Material.GOLDEN_HOE);
-        hoes.add(Material.DIAMOND_HOE);
-    }
 
     public VeinMinerListener(ModdedAdditions instance) {
         this.accessController = instance.getBlockAccessController();
--- src/main/java/xyz/fluxinc/moddedadditions/storage/Tools.java
+++ src/main/java/xyz/fluxinc/moddedadditions/storage/Tools.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class Tools {
 
     public static List<Material> pickaxes;
+    public static List<Material> axes;
+    public static List<Material> shovels;
+    public static List<Material> hoes;
     static {
         pickaxes = new ArrayList<>();
         pickaxes.add(Material.WOODEN_PICKAXE);
@@ -15,5 +18,26 @@ public class Tools {
         pickaxes.add(Material.IRON_PICKAXE);
         pickaxes.add(Material.GOLDEN_PICKAXE);
         pickaxes.add(Material.DIAMOND_PICKAXE);
+
+        axes = new ArrayList<>();
+        axes.add(Material.WOODEN_AXE);
+        axes.add(Material.STONE_AXE);
+        axes.add(Material.IRON_AXE);
+        axes.add(Material.GOLDEN_AXE);
+        axes.add(Material.DIAMOND_AXE);
+
+        shovels = new ArrayList<>();
+        shovels.add(Material.WOODEN_SHOVEL);
+        shovels.add(Material.STONE_SHOVEL);
+        shovels.add(Material.IRON_SHOVEL);
+        shovels.add(Material.GOLDEN_SHOVEL);
+        shovels.add(Material.DIAMOND_SHOVEL);
+
+        hoes = new ArrayList<>();
+        hoes.add(Material.WOODEN_HOE);
+        hoes.add(Material.STONE_HOE);
+        hoes.add(Material.IRON_HOE);
+        hoes.add(Material.GOLDEN_HOE);
+        hoes.add(Material.DIAMOND_HOE);
     }
 }
