5b2ac32791e61b1cb96f8e9abaaf17b72ebb0dff
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-01-10 13:29:27 +0000

    Optimised imports

--- src/main/java/xyz/fluxinc/moddedadditions/listeners/customitem/LightSaberAttackListener.java
+++ src/main/java/xyz/fluxinc/moddedadditions/listeners/customitem/LightSaberAttackListener.java
@@ -8,7 +8,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import xyz.fluxinc.fluxcore.utils.LoreUtils;
 import xyz.fluxinc.moddedadditions.ModdedAdditions;
 
 public class LightSaberAttackListener implements Listener {
