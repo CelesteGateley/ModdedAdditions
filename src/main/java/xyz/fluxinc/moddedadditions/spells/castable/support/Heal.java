41abecd6e456653a387cb79ad0484cf845c107bc
BuildTools <unconfigured@null.spigotmc.org>
2020-07-29 00:48:29 +0100

    Fixed Heal spell implementation for multiple levels

--- src/main/java/xyz/fluxinc/moddedadditions/spells/castable/support/Heal.java
+++ src/main/java/xyz/fluxinc/moddedadditions/spells/castable/support/Heal.java
@@ -81,40 +81,15 @@ public class Heal extends Spell {
             case 2:
                 ++heal;
         }
-        System.out.println(target);
-        List<Entity> targets = getNear(caster, target, level);
-        System.out.println(heal);
-        System.out.println(targets);
+        List<LivingEntity> targets = level == 4 ? getNearbyEntities(target, 5) : new ArrayList<>();
+        targets.add(target);
+
         int counter = 0;
-        for (Entity E : targets) {
-            System.out.println(E.getType());
-            if (E instanceof Mob) {
-                System.out.println(((Mob) E).getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-                System.out.println(((Mob) E).getHealth());
-
-
-                if (((Mob) E).getHealth() != ((Mob) E).getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()){
-                    System.out.println("Need Healing");
-                    if (((Mob) E).getHealth() > ((Mob) E).getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() - heal) {
-                        System.out.println("Max heal");
-                        ((Mob) E).setHealth(((Mob) E).getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-                    }
-                    else {
-                        System.out.println("Standard heal");
-                    ((Mob) E).setHealth(((Mob) E).getHealth()+heal);
-                    }
-                    counter ++;
-                }
-                if (E instanceof Player && ((Player) E).getFoodLevel() < 20) {
-                    if (((Player) E).getFoodLevel() <= 20 - heal) {
-                        ((Player) E).setFoodLevel(((Player) E).getFoodLevel() + heal);
-                    } else {
-                        ((Player) E).setFoodLevel(20);
-                    }
-                    counter++;
-                }
-                continue;
-            }
+        for (LivingEntity entity : targets) {
+            boolean hungerCheck = true;
+            if (entity instanceof Player)  hungerCheck = ((Player) entity).getFoodLevel() >= 20;
+            if (entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() == entity.getHealth() && hungerCheck) continue;
+            if (healTarget(entity, heal)) counter++;
         }
         if (counter > 0) {
             target.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, target.getLocation(), 50, 0.5, 1, 0.5);
@@ -126,40 +101,32 @@ public class Heal extends Spell {
         }
     }
 
-    public List<Entity> getNear(Player caster, LivingEntity target, int level) {
-
-        ArrayList<Entity> entities;
-        ArrayList<Entity> toremove = new ArrayList<Entity>() {
-        };
-
-        if (level == 4) {
-            entities = (ArrayList<Entity>) caster.getWorld().getNearbyEntities(caster.getLocation(), 5, 5, 5);
-            for (Entity E:entities){
-                if (!(((E instanceof Player || E instanceof Mob) && (((LivingEntity) E).getHealth() < ((LivingEntity) E).getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue())))) {
-                    toremove.add(E);
-                }
-                if (E instanceof Monster || E instanceof Flying || E instanceof Slime) {
-                    toremove.add(E);
-                }
-            }
-            entities.removeAll(toremove);
-        }
-
-        else {
-            entities = new ArrayList<>();
-            if (caster != target) {
-                caster.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("You heal your targets wounds"));
-                if (target instanceof Player) {
-                    ((Player) target).spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("You feel your wounds mend"));
-                    entities.add(target);
-                }
-            } else {
-                caster.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("You feel your wounds mend"));
-                entities.add(caster);
-            }
-
+    private List<LivingEntity> getNearbyEntities(LivingEntity target, int distance) {
+        List<LivingEntity> entities = new ArrayList<>();
+        for (Entity entity : target.getNearbyEntities(distance, distance, distance)) {
+            if (!(entity instanceof LivingEntity)) continue;
+            if (entity instanceof Monster) continue;
+            entities.add((LivingEntity) entity);
         }
         return entities;
+    }
+
+    private boolean healTarget(LivingEntity target, int amount) {
+        boolean doneHunger = true;
+        boolean doneHealth = true;
+
+        if (target instanceof Player) {
+            if (((Player) target).getFoodLevel() == 20) { doneHunger = false; }
+            else if (((Player) target).getFoodLevel() >= 20-amount) { ((Player) target).setFoodLevel(20); }
+            else { ((Player) target).setFoodLevel(((Player) target).getFoodLevel() + amount);}
+        }
+
+        if (target.getHealth() == target.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()) { doneHealth = false;}
+        else if (target.getHealth() >= target.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()-amount) { target.setHealth(target.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()); }
+        else { target.setHealth(target.getHealth() + amount); }
+
+        return doneHealth || doneHunger;
+
     }
 
 
