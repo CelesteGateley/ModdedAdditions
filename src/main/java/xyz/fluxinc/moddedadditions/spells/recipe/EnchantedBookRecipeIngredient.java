69f1e4e2841c5daefde1bcb923e3310a632c8980
CelesteMagisteel <kamg1134@gmail.com>
2020-08-02 21:13:43 +0100

    Fixed EnchantedBooks not working for enchantment recipe items

--- src/main/java/xyz/fluxinc/moddedadditions/spells/recipe/EnchantedBookRecipeIngredient.java
+++ src/main/java/xyz/fluxinc/moddedadditions/spells/recipe/EnchantedBookRecipeIngredient.java
@@ -1,8 +1,8 @@
 package xyz.fluxinc.moddedadditions.spells.recipe;
 
-import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class EnchantedBookRecipeIngredient extends RecipeIngredient {
@@ -15,9 +15,9 @@ public class EnchantedBookRecipeIngredient extends RecipeIngredient {
 
     @Override
     public boolean verifyItem(ItemStack itemStack) {
-        if (itemStack.getType() != Material.ENCHANTED_BOOK) return false;
         ItemMeta itemMeta = itemStack.getItemMeta();
-        if (itemMeta == null) return false;
-        return itemMeta.getEnchants().containsKey(type);
+        if (itemMeta.getEnchants().containsKey(type)) return true;
+        if (!(itemMeta instanceof EnchantmentStorageMeta)) return false;
+        return ((EnchantmentStorageMeta) itemMeta).getStoredEnchants().containsKey(type);
     }
 }
