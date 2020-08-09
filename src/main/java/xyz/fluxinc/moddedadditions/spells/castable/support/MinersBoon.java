94d50c03f189d52929c4c163984426afb57b3efb
CelesteMagisteel <kamg1134@gmail.com>
2020-08-09 13:53:20 +0100

    Added second level riddle for miners boon

--- src/main/java/xyz/fluxinc/moddedadditions/spells/castable/support/MinersBoon.java
+++ src/main/java/xyz/fluxinc/moddedadditions/spells/castable/support/MinersBoon.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -15,6 +16,7 @@ import xyz.fluxinc.moddedadditions.ModdedAdditions;
 import xyz.fluxinc.moddedadditions.controllers.customitems.SpellBookController;
 import xyz.fluxinc.moddedadditions.spells.Spell;
 import xyz.fluxinc.moddedadditions.spells.SpellRecipe;
+import xyz.fluxinc.moddedadditions.spells.recipe.EnchantedBookRecipeIngredient;
 import xyz.fluxinc.moddedadditions.spells.recipe.MaterialRecipeIngredient;
 
 import static xyz.fluxinc.fluxcore.utils.LoreUtils.addLore;
@@ -62,6 +64,8 @@ public class MinersBoon extends Spell {
         switch (level) {
             case 0:
                 return "&9Catalyst: &4Redstone\n\n&9An ancient metal, and a buried heart, grants one the strength of the earth";
+            case 1:
+                return "&9Catalyst: &cGlowstone Dust\n\n&9An ancient pickaxe, efficient and everlasting, self repairing for eternity";
             default:
                 return null;
         }
@@ -79,6 +83,10 @@ public class MinersBoon extends Spell {
             case 0:
                 return new SpellRecipe(this, new MaterialRecipeIngredient(Material.REDSTONE),
                         new MaterialRecipeIngredient(Material.NETHERITE_INGOT), new MaterialRecipeIngredient(Material.HEART_OF_THE_SEA));
+            case 1:
+                return new SpellRecipe(this, new MaterialRecipeIngredient(Material.GLOWSTONE_DUST),
+                        new MaterialRecipeIngredient(Material.NETHERITE_PICKAXE), new EnchantedBookRecipeIngredient(Enchantment.DIG_SPEED),
+                        new EnchantedBookRecipeIngredient(Enchantment.DURABILITY), new EnchantedBookRecipeIngredient(Enchantment.MENDING));
             default:
                 return null;
         }
