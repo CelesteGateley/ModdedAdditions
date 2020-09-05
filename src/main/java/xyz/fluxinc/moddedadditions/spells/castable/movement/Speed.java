a44e86aafb90934827769650ed039b011fe45501
CelesteMagisteel <kamg1134@gmail.com>
2020-09-05 15:36:52 +0100

    Added level 3 speed riddle

--- src/main/java/xyz/fluxinc/moddedadditions/spells/castable/movement/Speed.java
+++ src/main/java/xyz/fluxinc/moddedadditions/spells/castable/movement/Speed.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -14,6 +15,7 @@ import xyz.fluxinc.moddedadditions.ModdedAdditions;
 import xyz.fluxinc.moddedadditions.controllers.customitems.SpellBookController;
 import xyz.fluxinc.moddedadditions.spells.Spell;
 import xyz.fluxinc.moddedadditions.spells.SpellRecipe;
+import xyz.fluxinc.moddedadditions.spells.recipe.EnchantedBookRecipeIngredient;
 import xyz.fluxinc.moddedadditions.spells.recipe.MaterialRecipeIngredient;
 import xyz.fluxinc.moddedadditions.spells.recipe.PotionRecipeIngredient;
 
@@ -65,6 +67,8 @@ public class Speed extends Spell {
                 return "&9Catalyst: &4Redstone\n\n&9Sweetening your life, this will make you faster than light";
             case 1:
                 return "&9Catalyst: &cGlowstone Dust\n\n&9A bottle of liquid speed held to you, combined with an elixir of health, given form within a liquid basin, and fermented with the eye of the dead";
+            case 2:
+                return "&9Catalyst: &cDiamond\n\n&9Faster flight, quicker than lightning, energetic beyond the norm, and riding high";
             default:
                 return null;
         }
@@ -86,6 +90,10 @@ public class Speed extends Spell {
                 return new SpellRecipe(this, new MaterialRecipeIngredient(Material.GLOWSTONE_DUST),
                         new PotionRecipeIngredient(PotionType.SPEED), new PotionRecipeIngredient(PotionType.INSTANT_HEAL),
                         new MaterialRecipeIngredient(Material.CAULDRON), new MaterialRecipeIngredient(Material.FERMENTED_SPIDER_EYE));
+            case 2:
+                return new SpellRecipe(this, new MaterialRecipeIngredient(Material.GLOWSTONE_DUST),
+                        new EnchantedBookRecipeIngredient(Enchantment.ARROW_DAMAGE), new EnchantedBookRecipeIngredient(Enchantment.CHANNELING),
+                        new MaterialRecipeIngredient(Material.SUGAR), new MaterialRecipeIngredient(Material.SADDLE));
             default:
                 return null;
         }
