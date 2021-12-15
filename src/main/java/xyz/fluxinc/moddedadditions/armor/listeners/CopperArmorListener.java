6eca9cab4073725d565af85168b96cf3fb2c9d27
Nerdy_Dragon <zhero240@gmail.com>
2021-12-16 00:53:11 +0200

    Cleans up CopperArmorListener.java

--- src/main/java/xyz/fluxinc/moddedadditions/armor/listeners/CopperArmorListener.java
+++ src/main/java/xyz/fluxinc/moddedadditions/armor/listeners/CopperArmorListener.java
@@ -1,12 +1,10 @@
 package xyz.fluxinc.moddedadditions.armor.listeners;
 
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
