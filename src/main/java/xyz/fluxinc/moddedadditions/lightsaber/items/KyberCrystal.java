7370edc876c1b5636fe4f097e0c6876bcd26eca4
Celeste Gateley <celeste.gateley@gmail.com>
2022-01-06 16:44:38 +0000

    Fix for logic on saber getColor methods

--- src/main/java/xyz/fluxinc/moddedadditions/lightsaber/items/KyberCrystal.java
+++ src/main/java/xyz/fluxinc/moddedadditions/lightsaber/items/KyberCrystal.java
@@ -37,8 +37,8 @@ public class KyberCrystal extends CustomItem {
     public static SaberColor getColor(ItemStack itemStack) {
         ItemMeta iMeta = itemStack.getItemMeta();
         if (iMeta == null || iMeta.hasCustomModelData()) return null;
-        if (itemStack.getItemMeta().getCustomModelData() < KEY_BASE + KYBER_CRYSTAL_BASE
-                && itemStack.getItemMeta().getCustomModelData() > KEY_BASE + KYBER_CRYSTAL_BASE + 10) return null;
+        if (itemStack.getItemMeta().getCustomModelData() >= KEY_BASE + KYBER_CRYSTAL_BASE
+                && itemStack.getItemMeta().getCustomModelData() < KEY_BASE + KYBER_CRYSTAL_BASE + 10) return null;
         return SaberColor.getModColor(iMeta.getCustomModelData() - KEY_BASE - KYBER_CRYSTAL_BASE);
     }
 
