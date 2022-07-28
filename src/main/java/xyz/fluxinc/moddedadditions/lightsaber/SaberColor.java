40a92333eea69682f6f335c4564a893004a4019d
Celeste Gateley <celeste.gateley@gmail.com>
2022-07-29 00:18:53 +0100

    Fix for SaberColor glass

--- src/main/java/xyz/fluxinc/moddedadditions/lightsaber/SaberColor.java
+++ src/main/java/xyz/fluxinc/moddedadditions/lightsaber/SaberColor.java
@@ -7,13 +7,13 @@ public enum SaberColor {
 
     DEPLETED(ChatColor.RESET, 0, Material.BEDROCK), // Default value used instead of null
     BLUE(ChatColor.AQUA, 1, Material.LIGHT_BLUE_STAINED_GLASS),
-    GREEN(ChatColor.GREEN, 2, Material.LIGHT_BLUE_STAINED_GLASS),
-    PURPLE(ChatColor.DARK_PURPLE, 3, Material.LIGHT_BLUE_STAINED_GLASS),
-    RED(ChatColor.DARK_RED, 4, Material.LIGHT_BLUE_STAINED_GLASS),
-    YELLOW(ChatColor.YELLOW, 5, Material.LIGHT_BLUE_STAINED_GLASS),
-    ORANGE(ChatColor.GOLD, 6, Material.LIGHT_BLUE_STAINED_GLASS),
-    WHITE(ChatColor.WHITE, 7, Material.LIGHT_BLUE_STAINED_GLASS),
-    DARK(ChatColor.DARK_GRAY, 8, Material.LIGHT_BLUE_STAINED_GLASS);
+    GREEN(ChatColor.GREEN, 2, Material.LIME_STAINED_GLASS),
+    PURPLE(ChatColor.DARK_PURPLE, 3, Material.PURPLE_STAINED_GLASS),
+    RED(ChatColor.DARK_RED, 4, Material.RED_STAINED_GLASS),
+    YELLOW(ChatColor.YELLOW, 5, Material.YELLOW_STAINED_GLASS),
+    ORANGE(ChatColor.GOLD, 6, Material.ORANGE_STAINED_GLASS),
+    WHITE(ChatColor.WHITE, 7, Material.WHITE_STAINED_GLASS),
+    DARK(ChatColor.DARK_GRAY, 8, Material.BARRIER);
 
     public final ChatColor chatColor;
     public final int model;
